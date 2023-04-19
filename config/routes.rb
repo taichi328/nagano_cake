5a19e3b3a1b8a41accf1900f0e22e9bede985886
EC2 Default User <125885895+taichi328@users.noreply.github.com>
2023-04-19 13:18:23 +0000

    [add]Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-root to: "homes#top"
+root to: "public/homes#top"
 end
